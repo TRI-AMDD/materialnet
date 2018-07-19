e95c9a44920b9f3ab01d09e1b0be732b212eb193
Roni Choudhury <roni.choudhury@kitware.com>
2018-07-19 17:29:44 -0400

    Add a script to help process down the raw data

---
+++ data/process.py
@@ -0,0 +1,34 @@
+import csv
+import json
+import sys
+
+
+def unique_nodes(edges):
+    nodes = set([]);
+    for e in edges:
+        nodes.add(e[0])
+        nodes.add(e[1])
+
+    return nodes
+
+
+def filter_edges(edges, nodes):
+    return filter(lambda e: e[0] in nodes and e[1] in nodes, edges)
+
+
+def main():
+    reader = csv.reader(sys.stdin)
+    edges = list(reader)
+
+    nodes = unique_nodes(edges)
+
+    print '%d unique nodes' % (len(nodes))
+
+    node_sample = set(list(nodes)[:100])
+
+    edge_sample = filter_edges(edges, node_sample)
+    print json.dumps(edge_sample, indent=2)
+
+
+if __name__ == '__main__':
+    sys.exit(main())
