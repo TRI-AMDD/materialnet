f0ca090ccdfcc6668df782608c5fbd69fbebcb05
Roni Choudhury <roni.choudhury@kitware.com>
2018-07-20 16:18:55 -0400

    Add basic animation of graph

--- src/index.js
+++ src/index.js
@@ -1,14 +1,62 @@
 import * as three from 'three';
 import { select } from 'd3-selection';
+import * as cola from 'webcola';
 
 import html from './index.pug';
+import edges from './edges.json';
+
+function computeGraph (edges) {
+  const radius = 20;
+
+  // Create a node index table.
+  let index = {};
+  let circles = [];
+  let count = 0;
+  edges.forEach(e => {
+    // Create an entry if the node hasn't been seen yet.
+    e.forEach(n => {
+      if (!index.hasOwnProperty(n)) {
+        // Record the index.
+        index[n] = count++;
+
+        // Create a circle object.
+        const circ = new Circle();
+        circ.radius = radius;
+
+        circles.push(circ);
+      }
+    });
+  });
+
+  // Create a version of the edges list that has indices instead of names.
+  const edgeIndex = edges.map(e => e.map(n => index[n]))
+
+  // Create a Cola layout to manipulate the objects.
+  const layout = new cola.Layout()
+    .linkDistance(3 * radius)
+    .avoidOverlaps(true)
+    .links(edgeIndex.map(e => ({
+      source: e[0],
+      target: e[1]
+    })));
+  layout.nodes().forEach(n => {
+    n.width = n.height = 2 * radius;
+  });
+
+  return {
+    nodeIndex: index,
+    edgeIndex,
+    circles,
+    layout
+  };
+}
 
 const circleGeometry = new three.CircleBufferGeometry(1, 32);
 
 class Circle {
-  constructor ({color}) {
+  constructor () {
     this.material = new three.MeshLambertMaterial({
-      color
+      color: 0xffffff
     });
 
     this.circle = new three.Mesh(circleGeometry, this.material);
@@ -46,66 +94,31 @@ const renderer = new three.WebGLRenderer();
 renderer.setSize(width, height);
 select('#vis').append(() => renderer.domElement);
 
-const circle = new Circle({
-  color: 0xff0000
-});
-circle.radius = 80;
-circle.position.x = 200;
-circle.position.y = 200;
-circle.addToScene(scene);
-
-const circle2 = new Circle({
-  color: 0x00ff00,
-  scene
-});
-circle2.position.x = 200;
-circle2.position.y = -200;
-circle2.radius = 25;
-circle2.addToScene(scene);
-
-const circle3 = new Circle({
-  color: 0xddaa22,
-  scene
-});
-circle3.position.x = 200;
-circle3.position.y = -100;
-circle3.radius = 25;
-circle3.addToScene(scene);
-
 const dirLight = new three.DirectionalLight();
 dirLight.position.x = 0;
 dirLight.position.y = 0;
 dirLight.position.z = 200;
 scene.add(dirLight);
 
-let factor = 1;
-let speed = 1.8;
-let speedFactor = 1;
-function animate () {
-  requestAnimationFrame(animate);
+// Prepare the graph from the edge data..
+const graph = computeGraph(edges);
 
-  circle2.color.b += factor * 0.01;
-  if(circle2.color.b >= 1.0) {
-    factor = -1;
-  } else if (circle2.color.b <= 0.0) {
-    factor = 1;
-  }
+// Animate the graph.
+graph.circles.forEach(c => c.addToScene(scene));
 
-  circle.position.x -= 1;
-
-  circle2.position.x -= 2;
-  circle2.position.y += speed;
-
-  circle3.position.x -= 1;
-  circle3.position.y -= speed;
-
-  speed += speedFactor * 0.1;
-  if (speed <= -1.8) {
-    speedFactor = 1;
-  } else if (speed >= 1.8) {
-    speedFactor = -1;
-  }
-
+function animate (e) {
+  graph.layout.tick();
   renderer.render(scene, camera);
+
+  requestAnimationFrame(animate);
 }
+
+graph.layout.start(1,0,0,0,false);
+graph.layout.on(cola.EventType.tick, e => {
+  graph.layout.nodes().forEach((n, i) => {
+    graph.circles[i].position.x = n.x;
+    graph.circles[i].position.y = n.y;
+  });
+});
+
 animate();
