15b87ac331e10c79c057d7062aff491314d458cb
Roni Choudhury <roni.choudhury@kitware.com>
2018-07-31 11:07:47 -0400

    Draw a stroke around the nodes

--- src/circle-frag.glsl
+++ src/circle-frag.glsl
@@ -1,9 +1,13 @@
 varying vec4 vColor;
 
 void main() {
-    float f = length(gl_PointCoord - vec2(0.5, 0.5));
-    if (f > 0.5) {
-        discard;
-    }
+  float f = length(gl_PointCoord - vec2(0.5, 0.5));
+  if (f > 0.5) {
+    discard;
+  } else if (f > 0.4) {
+    float factor = abs(0.5 - f) * 10.0;
+    gl_FragColor = mix(vec4(0.0, 0.0, 0.0, 1.0), vColor, factor);
+  } else {
     gl_FragColor = vColor;
+  }
 }
