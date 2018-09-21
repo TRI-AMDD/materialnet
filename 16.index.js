7fa43c195b671f1ab6b1593ead8d148ca41167b6
Roni Choudhury <roni.choudhury@kitware.com>
2018-09-21 10:58:20 -0400

    release from "space" branch

---
+++ 16.index.js
@@ -0,0 +1,15 @@
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([[16],{
+
+/***/ "./node_modules/@openchemistry/molecule/dist/esm/es5/polyfills/object.js":
+/*!*******************************************************************************!*\
+  !*** ./node_modules/@openchemistry/molecule/dist/esm/es5/polyfills/object.js ***!
+  \*******************************************************************************/
+/*! exports provided: applyPolyfill */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, \"applyPolyfill\", function() { return applyPolyfill; });\nfunction applyPolyfill(window, document) {\n/*!\nObject.assign\n*/\n\"function\"!=typeof Object.assign&&Object.defineProperty(Object,\"assign\",{value:function(d,f){if(null==d)throw new TypeError(\"Cannot convert undefined or null to object\");for(var e=Object(d),b=1;b<arguments.length;b++){var a=arguments[b];if(null!=a)for(var c in a)Object.prototype.hasOwnProperty.call(a,c)&&(e[c]=a[c])}return e},writable:!0,configurable:!0});\n/*!\nObject.entries\n*/\nObject.entries||(Object.entries=function(c){for(var b=Object.keys(c),a=b.length,d=Array(a);a--;)d[a]=[b[a],c[b[a]]];return d});\n}\n\n//# sourceURL=webpack:///./node_modules/@openchemistry/molecule/dist/esm/es5/polyfills/object.js?");
+
+/***/ })
+
+}]);
