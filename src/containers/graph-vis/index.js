6e184d34ad136fc1e891a5d151e0679b8b57fcad
Alessandro Genova <ales.genova@gmail.com>
2018-10-26 12:05:04 -0400

    Add app skeleton

---
+++ src/containers/graph-vis/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+import GraphVisComponent from '../../components/graph-vis';
+
+class GraphVisContainer extends Component {
+  render() {
+    return (
+      <GraphVisComponent/>
+    );
+  }
+}
+
+export default GraphVisContainer;
