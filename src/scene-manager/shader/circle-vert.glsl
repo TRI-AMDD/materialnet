f0e6637de14e81de69a913bc263b0963059127d5
Roni Choudhury <roni.choudhury@kitware.com>
2018-10-26 11:27:29 -0400

    Add sceneManager class

---
+++ src/scene-manager/shader/circle-vert.glsl
@@ -0,0 +1,16 @@
+varying vec4 vColor;
+attribute float size;
+uniform float zoom;
+attribute float selected;
+varying vec4 edgeColor;
+attribute float focus;
+attribute float hidden;
+varying float _hidden;
+
+void main() {
+  gl_PointSize = zoom * 0.5 * size;
+  gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
+  vColor = vec4(color, focus);
+  edgeColor = selected > 0.5 ? vec4(1.0, 1.0, 0.0, focus) : vec4(0.0, 0.0, 0.0, focus);
+  _hidden = hidden;
+}
