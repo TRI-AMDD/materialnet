b3630140cd3b1b8b7e6caf81efb8d1ec4e4038da
Alessandro Genova <ales.genova@gmail.com>
2018-10-26 11:05:34 -0400

    Add raw-loader for glsl files

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -304,6 +304,10 @@ module.exports = {
               'sass-loader'
             ),
           },
+          {
+            test: /\.glsl$/,
+            use: ['raw-loader']
+          },
           // "file" loader makes sure those assets get served by WebpackDevServer.
           // When you `import` an asset, you get its (virtual) filename.
           // In production, they would get copied to the `build` folder.
--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -394,6 +394,10 @@ module.exports = {
               'sass-loader'
             ),
           },
+          {
+            test: /\.glsl$/,
+            use: ['raw-loader']
+          },
           // "file" loader makes sure assets end up in the `build` folder.
           // When you `import` an asset, you get its filename.
           // This loader doesn't use a "test" so it will catch all modules
