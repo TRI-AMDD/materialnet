0a2dfa6c3cab2388f3dc70e940b27c016cb60b5c
Samuel Gratzl <sam@sgratzl.com>
2019-09-09 09:33:22 -0400

    better default template

--- src/datasets/cooccurence.js
+++ src/datasets/cooccurence.js
@@ -1,26 +1,49 @@
 import defaultTemplate from './default';
+import { propertyColorFactory, propertySizeFactory } from './utils';
+
+const properties = {
+    degree: {
+        label: 'Derived materials',
+        format: 'd'
+    },
+    deg_cent: {
+        label: "Degree Centrality",
+    },
+    eigen_cent: {
+        label: "Eigenvector Centrality"
+    },
+};
+
+const colors = [
+    ...defaultTemplate.colors,
+    ...Object.entries(properties).map(([prop, info]) => {
+        return {
+            label: info.label,
+            factory: propertyColorFactory(prop)
+        };
+    })
+]
+
+const sizes = [
+    ...defaultTemplate.sizes,
+    ...Object.entries(properties).map(([prop, info]) => {
+        return {
+            label: info.label,
+            factory: propertySizeFactory(prop)
+        };
+    })
+]
 
 export default {
     ...defaultTemplate,
-    yearRange: [1945, 2016],
     
-    properties: {
-        degree: {
-            label: 'Derived materials',
-            format: 'd'
-        },
-        deg_cent: {
-            label: "Degree Centrality",
-        },
-        eigen_cent: {
-            label: "Eigenvector Centrality"
-        },
-    },
+    properties,
+    colors,
+    sizes,
 
     defaults: {
         ...defaultTemplate.defaults,
-        zoom: -2.3,
-        colorYear: 2016,
-        year: 2016,
+        color: colors[1],
+        size: sizes[1]
     },
 }
