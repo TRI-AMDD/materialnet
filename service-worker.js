0b62979035c15935690aba830c2a8a27e4520145
Roni <roni.choudhury@kitware.com>
2019-10-04 16:53:31 +0000

    Automated deployment to GitHub Pages: 7241e8c4931d4b83c64ae29dbeacc2c808f2aaaf

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/materialnet/precache-manifest.f258421f04ac1138e319a513806c5e4b.js"
+  "/materialnet/precache-manifest.3e3ce55b86b40a25da9499f2ae63463d.js"
 );
 
 workbox.clientsClaim();
