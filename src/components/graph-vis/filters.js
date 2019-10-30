46dda4c4fe34f8bdf5531f8ff1a758a7c43bc7e8
Roni Choudhury <roni.choudhury@kitware.com>
2019-10-30 16:52:30 -0400

    Move discovery year slider to filtering panel

--- src/components/graph-vis/filters.js
+++ src/components/graph-vis/filters.js
@@ -4,9 +4,12 @@ import Grid from '../controls/grid';
 import Store from '../../store';
 import { observer } from 'mobx-react';
 import ReactSelectWrapper from '../controls/reactselect';
+import SliderControl from '../controls/slider';
 import RangeSliderControlComponent from '../controls/rangeslider';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faFlask } from '@fortawesome/free-solid-svg-icons';
+import { IconButton } from '@material-ui/core';
+import { PlayArrow, Pause } from '@material-ui/icons';
 
 @observer
 class Filters extends React.Component {
@@ -16,6 +19,21 @@ class Filters extends React.Component {
   render() {
     const store = this.context;
     return (<Grid>
+      { /* dataset specific */}
+      {store.yearRange && <SliderControl
+        value={store.year}
+        range={store.yearRange}
+        step={1}
+        label={'Discovered before'}
+        digits={0}
+        onChange={(val) => { store.year = val; }}
+      >
+        <IconButton
+          onClick={() => store.toggleAutoplay()}
+        >
+          {store.play ? <Pause /> : <PlayArrow />}
+        </IconButton>
+      </SliderControl>}
       {store.propertyList.filter((meta) => meta.filterable).map((meta) => <RangeSliderControlComponent
         key={meta.label}
         value={store.filters[meta.property] ? store.filters[meta.property].slice() : meta.domain.slice()}
