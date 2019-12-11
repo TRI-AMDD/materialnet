abfa5d182085a189cdc007ae6e24f5366b64be26
Roni Choudhury <roni.choudhury@kitware.com>
2019-12-11 17:51:11 -0500

    Add about page content

--- src/components/header/AboutPage.js
+++ src/components/header/AboutPage.js
@@ -6,6 +6,8 @@ import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
 import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogTitle from '@material-ui/core/DialogTitle';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faQuestionCircle } from '@fortawesome/free-solid-svg-icons';
 
 const useStyles = makeStyles(theme => ({
   form: {
@@ -52,7 +54,7 @@ export default function AboutPage() {
       </Button>
       <Dialog
         fullWidth={true}
-        maxWidth={"xl"}
+        maxWidth={"md"}
         open={open}
         onClose={handleClose}
         aria-labelledby="max-width-dialog-title"
@@ -62,9 +64,33 @@ export default function AboutPage() {
         <DialogContent>
           <DialogContentText>
 
-            <h1>Blah</h1>
+            <p>
+              MaterialNet is an open-source web application for loading,
+              visualizing, and exploring the relationships between entities in a
+              materials database. To get started, try interacting with the map
+              of materials in the main display, and see what the controls in the
+              left panel do. Help is available throughout the application via
+              the <FontAwesomeIcon icon={faQuestionCircle} /> icons.
+            </p>
 
-            <p>Lorem ipsum blah di blah do blah.</p>
+            <p>
+              MaterialNet is released under the <a
+              href="https://www.apache.org/licenses/LICENSE-2.0.txt">Apache
+              License 2.0</a>, and the <a
+              href="https://github.com/arclamp/materialnet">source code</a> is
+              available for free on GitHub. If you run into trouble with the
+              software, you can <a href="">file an issue</a> or <a href="">open
+              a pull request</a>.
+            </p>
+
+            <p>
+              MaterialNet is a research collaboration between the <a
+              href="https://www.tri.global/">Toyota Research Institute</a> and <a
+              href="https://www.kitware.com/">Kitware Inc.</a> We are
+              committed to open science and the open source software that
+              supports it. If you feel we can help you solve your problems,
+              please do not hesitate to contact us.
+            </p>
 
           </DialogContentText>
         </DialogContent>
