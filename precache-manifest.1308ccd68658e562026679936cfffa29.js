52dfffe55544208254e3985a3e920535bba85368
Roni Choudhury <roni.choudhury@kitware.com>
2019-12-12 14:45:42 -0500

    Updates

---
+++ precache-manifest.1308ccd68658e562026679936cfffa29.js
@@ -0,0 +1,175 @@
+self.__precacheManifest = [
+  {
+    "revision": "e541a370b5870bb53c55",
+    "url": "/materialnet/static/js/0.e541a370.chunk.js"
+  },
+  {
+    "revision": "aa165e61c00315fd87ba",
+    "url": "/materialnet/static/js/1.aa165e61.chunk.js"
+  },
+  {
+    "revision": "f7e8565862c57c85a47a",
+    "url": "/materialnet/static/js/2.f7e85658.chunk.js"
+  },
+  {
+    "revision": "69cf799125330ea97c60",
+    "url": "/materialnet/static/js/3.69cf7991.chunk.js"
+  },
+  {
+    "revision": "5315b1c8b26fce7d99e8",
+    "url": "/materialnet/static/js/4.5315b1c8.chunk.js"
+  },
+  {
+    "revision": "fe17ca0fc14051858805",
+    "url": "/materialnet/static/js/5.fe17ca0f.chunk.js"
+  },
+  {
+    "revision": "92aef76ef6dd8eed4a85",
+    "url": "/materialnet/static/js/6.92aef76e.chunk.js"
+  },
+  {
+    "revision": "18139a63a226d7e7a1de",
+    "url": "/materialnet/static/js/7.18139a63.chunk.js"
+  },
+  {
+    "revision": "4f66acaaadfb28c90d2b",
+    "url": "/materialnet/static/js/8.4f66acaa.chunk.js"
+  },
+  {
+    "revision": "e7c128640a1a0df58b70",
+    "url": "/materialnet/static/js/9.e7c12864.chunk.js"
+  },
+  {
+    "revision": "30682ef6600339aaa0d4",
+    "url": "/materialnet/static/js/10.30682ef6.chunk.js"
+  },
+  {
+    "revision": "fbd47fb9dc818c71e6d6",
+    "url": "/materialnet/static/css/main.bd3f93b6.chunk.css"
+  },
+  {
+    "revision": "fbd47fb9dc818c71e6d6",
+    "url": "/materialnet/static/js/main.fbd47fb9.chunk.js"
+  },
+  {
+    "revision": "6e871c8c1b8e9d698c26",
+    "url": "/materialnet/static/js/12.6e871c8c.chunk.js"
+  },
+  {
+    "revision": "0f97c673335205df042a",
+    "url": "/materialnet/static/js/13.0f97c673.chunk.js"
+  },
+  {
+    "revision": "dce1affbc6c153ff86e1",
+    "url": "/materialnet/static/js/14.dce1affb.chunk.js"
+  },
+  {
+    "revision": "3bb94f21ece1d416e624",
+    "url": "/materialnet/static/js/15.3bb94f21.chunk.js"
+  },
+  {
+    "revision": "d368cbb83d76ee5dd4e0",
+    "url": "/materialnet/static/js/16.d368cbb8.chunk.js"
+  },
+  {
+    "revision": "cc75a4517fbfe62060a3",
+    "url": "/materialnet/static/js/17.cc75a451.chunk.js"
+  },
+  {
+    "revision": "1bd46f344293f93aed6f",
+    "url": "/materialnet/static/js/18.1bd46f34.chunk.js"
+  },
+  {
+    "revision": "a904f7af5b38fb57ff2a",
+    "url": "/materialnet/static/js/19.a904f7af.chunk.js"
+  },
+  {
+    "revision": "3de58a0c7e943f46f5d8",
+    "url": "/materialnet/static/js/20.3de58a0c.chunk.js"
+  },
+  {
+    "revision": "7e624d880c6acdbfff31",
+    "url": "/materialnet/static/js/21.7e624d88.chunk.js"
+  },
+  {
+    "revision": "71b2bb4f5252d570cf5d",
+    "url": "/materialnet/static/js/22.71b2bb4f.chunk.js"
+  },
+  {
+    "revision": "5aaef37375087591bb0d",
+    "url": "/materialnet/static/js/23.5aaef373.chunk.js"
+  },
+  {
+    "revision": "4b062d0d26aa2b424a7e",
+    "url": "/materialnet/static/js/24.4b062d0d.chunk.js"
+  },
+  {
+    "revision": "0b14ea4806fefeaeee8e",
+    "url": "/materialnet/static/js/25.0b14ea48.chunk.js"
+  },
+  {
+    "revision": "ae84dcfcfb88e293190a",
+    "url": "/materialnet/static/js/26.ae84dcfc.chunk.js"
+  },
+  {
+    "revision": "3b2f414bf2c1d7c4dddc",
+    "url": "/materialnet/static/js/27.3b2f414b.chunk.js"
+  },
+  {
+    "revision": "93541fef4c1a3ad0dc8e",
+    "url": "/materialnet/static/js/28.93541fef.chunk.js"
+  },
+  {
+    "revision": "1a3c8527bad04f03a7a8",
+    "url": "/materialnet/static/js/29.1a3c8527.chunk.js"
+  },
+  {
+    "revision": "3e79122523c3cae39f07",
+    "url": "/materialnet/static/js/30.3e791225.chunk.js"
+  },
+  {
+    "revision": "340084f0e38af23f0ace",
+    "url": "/materialnet/static/js/31.340084f0.chunk.js"
+  },
+  {
+    "revision": "6a55b5e898dae7c411f5",
+    "url": "/materialnet/static/js/32.6a55b5e8.chunk.js"
+  },
+  {
+    "revision": "71651cdc47f3d7d8c7b3",
+    "url": "/materialnet/static/js/33.71651cdc.chunk.js"
+  },
+  {
+    "revision": "d87e189da4283535125d",
+    "url": "/materialnet/static/js/34.d87e189d.chunk.js"
+  },
+  {
+    "revision": "cbbfa2c175bea113c4f5",
+    "url": "/materialnet/static/css/35.e2ac0042.chunk.css"
+  },
+  {
+    "revision": "cbbfa2c175bea113c4f5",
+    "url": "/materialnet/static/js/35.cbbfa2c1.chunk.js"
+  },
+  {
+    "revision": "c74cf36fe8d8348a4ad0",
+    "url": "/materialnet/static/js/runtime~main.c74cf36f.js"
+  },
+  {
+    "revision": "c462975a4480c0151e3553b1f25cce7c",
+    "url": "/materialnet/82afec16769321fce590.worker.js"
+  },
+  {
+    "url": "/materialnet/static/media/29334f20971b9251d88c2b7c1a082917.29334f20.eot"
+  },
+  {
+    "url": "/materialnet/static/media/6dbea47babde65521717314b0f38d514.6dbea47b.ttf"
+  },
+  {
+    "url": "/materialnet/static/media/ccda38f2ae7b5f07875dc4bba68b9586.ccda38f2.svg"
+  },
+  {
+    "revision": "c15a6b4295c481af726091790b5055a1",
+    "url": "/materialnet/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/materialnet/precache-manifest.3865d1fbc16fd07cfa4a1db98b25c1b7.js"
+  "/materialnet/precache-manifest.1308ccd68658e562026679936cfffa29.js"
 );
 
 workbox.clientsClaim();
@@ -30,5 +30,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute("/materialnet/index.html", {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
