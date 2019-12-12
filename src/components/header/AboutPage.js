e7d3e52235b0407fa126a84f2f1c2cdac3194431
Roni Choudhury <roni.choudhury@kitware.com>
2019-12-12 14:29:56 -0500

    Use correct React components

--- src/components/header/AboutPage.js
+++ src/components/header/AboutPage.js
@@ -35,48 +35,44 @@ export default function AboutPage() {
 
         <DialogContent>
           <DialogContentText>
+            MaterialNet is an open-source web application for loading,
+            visualizing, and exploring the relationships between entities in a
+            materials database. To get started, try interacting with the map
+            of materials in the main display, and see what the controls in the
+            left panel do. Help is available throughout the application via
+            the <FontAwesomeIcon icon={faQuestionCircle} /> icons.
+          </DialogContentText>
 
-            <p>
-              MaterialNet is an open-source web application for loading,
-              visualizing, and exploring the relationships between entities in a
-              materials database. To get started, try interacting with the map
-              of materials in the main display, and see what the controls in the
-              left panel do. Help is available throughout the application via
-              the <FontAwesomeIcon icon={faQuestionCircle} /> icons.
-            </p>
+          <DialogContentText>
+            In its current deployment, MaterialNet provides an interactive map
+            for the materials stability network (<a
+            href="https://doi.org/10.1038/s41467-019-10030-5" target="_blank"
+            rel="noopener noreferrer">Aykol et al. in Nat. Comm. 2019</a>) and
+            a text co-occurrence network network extracted from <a
+            href="http://www.matscholar.com" target="_blank" rel="noopener noreferrer">MatScholar</a>.
+          </DialogContentText>
 
-            <p>
-              In its current deployment, MaterialNet provides an interactive map
-              for the materials stability network (<a
-              href="https://doi.org/10.1038/s41467-019-10030-5" target="_blank"
-              rel="noopener noreferrer">Aykol et al. in Nat. Comm. 2019</a>) and
-              a text co-occurrence network network extracted from <a
-              href="http://www.matscholar.com" target="_blank" rel="noopener noreferrer">MatScholar</a>.
-            </p>
+          <DialogContentText>
+            MaterialNet is released under the <a
+            href="https://www.apache.org/licenses/LICENSE-2.0.txt"
+            target="_blank" rel="noopener noreferrer">Apache License 2.0</a>,
+            and the <a href="https://github.com/arclamp/materialnet"
+            target="_blank" rel="noopener noreferrer">source code</a> is
+            available for free on GitHub. If you run into trouble with the
+            software, you can <a
+            href="https://github.com/arclamp/materialnet/issues"
+            target="_blank" rel="noopener noreferrer">file an issue</a> or <a
+            href="https://github.com/arclamp/materialnet/pulls"
+            target="_blank" rel="noopener noreferrer">open a pull request</a>.
+          </DialogContentText>
 
-            <p>
-              MaterialNet is released under the <a
-              href="https://www.apache.org/licenses/LICENSE-2.0.txt"
-              target="_blank" rel="noopener noreferrer">Apache License 2.0</a>,
-              and the <a href="https://github.com/arclamp/materialnet"
-              target="_blank" rel="noopener noreferrer">source code</a> is
-              available for free on GitHub. If you run into trouble with the
-              software, you can <a
-              href="https://github.com/arclamp/materialnet/issues"
-              target="_blank" rel="noopener noreferrer">file an issue</a> or <a
-              href="https://github.com/arclamp/materialnet/pulls"
-              target="_blank" rel="noopener noreferrer">open a pull request</a>.
-            </p>
-
-            <p>
-              MaterialNet is a research collaboration between the <a
-              href="https://www.tri.global/" target="_blank" rel="noopener noreferrer">Toyota Research Institute</a> and
-              <a href="https://www.kitware.com/" target="_blank" rel="noopener noreferrer">Kitware Inc.</a> We
-              are committed to open science and
-              the open source software that supports it. If you feel we can help
-              you solve your problems, please do not hesitate to contact us.
-            </p>
-
+          <DialogContentText>
+            MaterialNet is a research collaboration between the <a
+            href="https://www.tri.global/" target="_blank" rel="noopener noreferrer">Toyota Research Institute</a> and
+            <a href="https://www.kitware.com/" target="_blank" rel="noopener noreferrer">Kitware Inc.</a> We
+            are committed to open science and
+            the open source software that supports it. If you feel we can help
+            you solve your problems, please do not hesitate to contact us.
           </DialogContentText>
         </DialogContent>
 
