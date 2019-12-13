019b1d029e39d9967c25e0ee1cf9dd0b6f4237b9
Roni Choudhury <roni.choudhury@kitware.com>
2019-12-13 15:57:04 -0500

    Add privacy policy link

--- src/components/header/AboutPage.js
+++ src/components/header/AboutPage.js
@@ -69,12 +69,19 @@ export default function AboutPage() {
           <DialogContentText>
             MaterialNet is a research collaboration between the <a
             href="https://www.tri.global/" target="_blank"
-            rel="noopener noreferrer">Toyota Research Institute</a> and <a href="https://www.kitware.com/"
-            target="_blank" rel="noopener noreferrer">Kitware Inc.</a> We
-            are committed to open science and
-            the open source software that supports it. If you feel we can help
-            you solve your problems, please do not hesitate to <a
-            href="https://www.kitware.com/contact-us/" target="_blank" rel="noopener noreferrer">contact us</a>.
+            rel="noopener noreferrer">Toyota Research Institute</a> and <a
+            href="https://www.kitware.com/" target="_blank"
+            rel="noopener noreferrer">Kitware Inc.</a> We
+            are committed to open science and the open source software that
+            supports it. If you feel we can help you solve your problems, please
+            do not hesitate to <a
+            href="https://www.kitware.com/contact-us/"
+            target="_blank" rel="noopener noreferrer">contact us</a>.
+          </DialogContentText>
+
+          <DialogContentText>
+            <a href="https://www.tri.global/privacy-policy/" target="_blank"
+               rel="noopener noreferrer">Privacy policy</a>
           </DialogContentText>
         </DialogContent>
 
