50e1ee379f956fd56657d1098674dc8aa1ed4e6d
Chris Harris <chris.harris@kitware.com>
2020-01-14 16:46:16 -0500

    Update S3 deployment script

--- scripts/deploy_s3.sh
+++ scripts/deploy_s3.sh
@@ -1,2 +1,13 @@
 #!/bin/bash
-aws s3 sync --delete build/ s3://materialsnet/
+
+if [ -z "$S3_BUCKET" ]
+then
+  echo "Please set \$S3_BUCKET"
+  exit 1
+fi
+
+S3_URL="s3://$S3_BUCKET/materialnet/"
+
+echo "Uploading to $S3_URL"
+
+aws s3 sync --delete build/ $S3_URL
