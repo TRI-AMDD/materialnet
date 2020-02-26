df28d1bccd0d73339ac4a718293c0986b1db0c9b
Roni Choudhury <roni.choudhury@kitware.com>
2020-02-26 10:21:54 -0500

    Add "keys" to dataset specifiers

--- src/datasets/index.js
+++ src/datasets/index.js
@@ -5,21 +5,25 @@ import cooccurence from './cooccurence';
 
 export default [
     {
+        key: 'stability',
         label: 'Materials Stability Network',
         fileName: './sample-data/stability.json',
         ...precise
     },
     {
+        key: 'co1k',
         label: 'Materials Co-occurrence Network (~1000 edges)',
         fileName: './sample-data/cooccurrence-1k.json',
         ...cooccurence
     },
     {
+        key: 'co500k',
         label: 'Materials Co-occurrence Network (~500000 edges)',
         fileName: './sample-data/cooccurrence-500k.json',
         ...cooccurence
     },
     {
+        key: 'similarity',
         label: 'Materials Similarity Network',
         fileName: './sample-data/similarity.json',
         ...similarity
