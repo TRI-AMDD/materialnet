fb0ef98f26fa1057ab22437504addc39b85e29f9
Roni Choudhury <roni.choudhury@kitware.com>
2020-02-26 10:48:27 -0500

    Add a filter to control which datasets appear in the menu

--- src/rest/index.js
+++ src/rest/index.js
@@ -1,3 +1,18 @@
+export function fetchJSON (path) {
+  return fetch(path)
+    .then(async res => {
+      const text = await res.text();
+      let json;
+      try {
+        json = JSON.parse(text);
+      } catch (e) {
+        json = null;
+      }
+
+      return json;
+    });
+}
+
 export function fetchStructure(name) {
   return fetch(`sample-data/structures/${name}.cjson`)
     .then(async res => {
